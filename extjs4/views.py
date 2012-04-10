5f1913ec4b9b24212ad820dab7613efb07c9184d
Espen A. Kristiansen <post@espenak.net>
2012-04-10 12:48:22 +0200

    views.Extjs4AppView: Fixed the javascript file paths.

--- extjs4/views.py
+++ extjs4/views.py
@@ -6,9 +6,14 @@ class Extjs4AppView(View):
     template_name = "extjs4/apptemplate.django.html"
     title = 'ExtJS app'
     css_staticpath = 'extjs4/resources/css/ext-all.css'
+    appname = None
 
     def get(self, request, **kwargs):
+        if not self.appname:
+            raise ValueError('appname is required')
         data = {'title': self.title,
                 'css_staticpath': self.css_staticpath,
+                'appall_staticpath': '{appname}/app-all.js'.format(appname=self.appname),
+                'app_staticpath': '{appname}/app.js'.format(appname=self.appname),
                 'params': kwargs}
         return render(request, self.template_name, data)
