b6da11d8580452f181f5ad93976f4f41d33eea51
Espen A. Kristiansen <post@espenak.net>
2012-09-01 06:37:09 -0700

    Set EXTJS4_DEBUGFILE as documented.

--- extjs4/context_processors.py
+++ extjs4/context_processors.py
@@ -3,6 +3,6 @@ from django.conf import settings
 
 def extjs4(request):
     return {'EXTJS4_DEBUG': getattr(settings, 'EXTJS4_DEBUG', False),
-            'EXTJS4_DEBUGFILE': getattr(settings, 'EXTJS4_DEBUGFILE', 'extjs4/ext-dev.js'),
+            'EXTJS4_DEBUGFILE': getattr(settings, 'EXTJS4_DEBUGFILE', 'extjs4/ext-all-dev.js'),
             'EXTJS4_PRODFILE': getattr(settings, 'EXTJS4_PRODFILE', 'extjs4/ext.js')
             }
